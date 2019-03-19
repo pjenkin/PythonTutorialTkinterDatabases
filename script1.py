96f11d42397cf1cb6543301b699325d2f37ad9fb
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-19 07:44:20 +0000

    course notes added

--- script1.py
+++ script1.py
@@ -19,7 +19,7 @@ e1_value=StringVar()
 e1 = Entry(window, textvariable=e1_value)      # text input
 e1.grid(row=0, column=1)
 
-t1 = Text(window, height=1, width = 20)
+t1 = Text(window, height=1, width=20)
 t1.grid(row=0, column=2)
 
 window.mainloop()
