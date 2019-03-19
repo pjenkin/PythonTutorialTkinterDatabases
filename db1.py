e1c88a14e5c5a5ec71869b6766896ecf6320b9c2
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-19 11:40:53 +0000

    12-160 Update and delete SQLite table records

--- db1.py
+++ db1.py
@@ -32,6 +32,35 @@ def view():
     connection.close()
     return rows                 # return fetched data
 
+def delete(item):
+    connection = sqlite3.connect('lite.db')     # connection with new or existing SQLite db
+    cursor = connection.cursor()
+    # sql_string = f'DELETE FROM store WHERE item=\'{item}\'' # NB f prefix
+    sql_string = f'DELETE FROM store WHERE item=\'{item}\'' # NB f prefix
+    print(sql_string)
+    cursor.execute(sql_string)    # NB f prefix
+    # cursor.execute('DELETE FROM store WHERE item=?', (item,))
+    connection.commit()
+    connection.close()
 
+
+def update(item, quantity, price):
+    connection = sqlite3.connect('lite.db')     # connection with new or existing SQLite db
+    cursor = connection.cursor()
+    # sql_string = f'DELETE FROM store WHERE item=\'{item}\'' # NB f prefix
+    sql_string = f'UPDATE store SET quantity={quantity}, price={price} WHERE item=\'{item}\'' # NB f prefix
+    print(sql_string)
+    cursor.execute(sql_string)    # NB f prefix
+    # cursor.execute('DELETE FROM store WHERE item=?', (item,))     # no comma needed at end if >1 parameter (item1, item2)
+    connection.commit()
+    connection.close()
+
+#insert('Kitkat', 100, 0.75)
+update('Kitkat', 100, 0.5, )
 print(view())
-# insert('Marathon',100, 0.75)
+insert('Marathon', 100, 0.75)
+print(view())
+delete('Marathon')
+print(view())
+update('Kitkat', 200, 0.5, )
+print(view())
